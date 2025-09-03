4d11a5e0e4fa5a79d0d3e3d173e5658d5418e852
Luís Guilherme <luisguilhermesoares10012009@gmail.com>
2025-09-03 10:33:53 -0300

    Feat: Atividade 01 , finalizada

--- 01-Atividade/01-Atividade/Program.cs
+++ 01-Atividade/01-Atividade/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Luis!");
+int num1 = 10;
+int num2 = 20;
+int x = num1;
+/// Atribundo valores ///
+num1 = num2;
+num2 = x; 
+Console.WriteLine($"O primeiro número é "+num1);
+Console.WriteLine($"O segundo número é "+num2 );
