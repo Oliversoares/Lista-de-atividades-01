49de47698e3ff8d79e130c50ba76b64242d0efba
Oliversoares <luisguilhermesoares10012009@gmail.com>
2025-09-10 10:30:54 -0300

    Feat: Atividade 11 feita

---
+++ 11-Atividade/11-Atividade/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num1;
+int num2;
+Console.WriteLine("Digite dois números , diferentes :");
+num1 = int.Parse(Console.ReadLine());
+num2 = int.Parse(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine("O maior número é:" +num1);
+}
+else
+{
+    Console.WriteLine("O maior número é:" + num2);
+}
