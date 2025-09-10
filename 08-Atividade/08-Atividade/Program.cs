5d4ed298e78eb99a4d421b11c72a828d0a65e352
Oliversoares <luisguilhermesoares10012009@gmail.com>
2025-09-10 08:51:44 -0300

    Feat : Atividade 08 finalizada

---
+++ 08-Atividade/08-Atividade/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int quant; 
+Console.WriteLine("Quanta maçãs você vai comprar ?");
+quant = int.Parse(Console.ReadLine());
+if (quant >= 12)
+{
+    Console.WriteLine($"O valor a pagar é de: " + quant*1);
+}
+else 
+{
+    Console.WriteLine($"O valor a pagar é de: " + quant * 1.3);
+}
