cefa00b5a3c2eba432336fe14f53d80e328bd1d8
Oliversoares <luisguilhermesoares10012009@gmail.com>
2025-09-10 10:19:00 -0300

    Feat: Atividade 10 feita

---
+++ 10-Atividade/10-Atividade/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+
+int anoAtual ;
+int anoNascimento;
+int idade;
+Console.Write("Digite o ano atual: ");
+anoAtual = int.Parse(Console.ReadLine());
+
+Console.Write("Digite o ano de nascimento: ");
+anoNascimento = int.Parse(Console.ReadLine());
+
+idade = anoAtual - anoNascimento;
+
+Console.WriteLine($"\nIdade: {idade} anos");
+
+if (idade < 16)
+{
+    Console.WriteLine("Você NÃO é obrigado a votar.");
+}
+else if ((idade >= 16 && idade < 18) || idade > 70)
+{
+    Console.WriteLine("Seu voto é FACULTATIVO.");
+}
+else
+{
+    Console.WriteLine("Seu voto é OBRIGATÓRIO.");
+}
