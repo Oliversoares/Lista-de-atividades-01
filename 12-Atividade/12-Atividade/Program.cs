4085a8736e8e8c3e183bf58dec738d1fed97c8a0
Oliversoares <luisguilhermesoares10012009@gmail.com>
2025-09-10 11:44:37 -0300

    Feat: Atividade 12 finalizada

---
+++ 12-Atividade/12-Atividade/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using System.Runtime.Intrinsics.Arm;
+
+int num1;
+int num2;
+int num3;
+Console.WriteLine("Digite três números , diferentes :");
+num1 = int.Parse(Console.ReadLine());
+num2 = int.Parse(Console.ReadLine());
+num3 = int.Parse(Console.ReadLine());
+if (num1 < num2 && num2 < num3)
+{
+    Console.WriteLine("A ordem crescente dos números é: " +num1+","+num2+","+num3 );
+}
+
+else if (num1 < num3 && num3 < num2)
+{
+    Console.WriteLine("A ordem crescente dos números é: " +num1+","+num3+","+num2);
+}
+
+else if (num2 < num1 && num1 < num3)
+{
+    Console.WriteLine("A ordem crescente dos números é: " + num2 + "," + num1 + "," + num3);
+}
+
+else if (num2 < num3 && num3 < num1)
+{
+    Console.WriteLine("A ordem crescente dos números é: " + num2 + "," + num3 + "," + num1);
+}
+
+else if (num3 < num1 && num1 < num2)
+{
+    Console.WriteLine("A ordem crescente dos números é: " + num3 + "," + num1 + "," + num2);
+}
+
+else if (num3 < num2 && num2 < num1)
+{
+    Console.WriteLine("A ordem crescente dos números é: " + num3 + "," + num2 + "," + num1);
+}
