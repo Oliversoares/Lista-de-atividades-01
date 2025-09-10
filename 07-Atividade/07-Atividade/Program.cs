164178936c0336326e76cc8f80e1b976c073163d
Oliversoares <luisguilhermesoares10012009@gmail.com>
2025-09-10 08:34:11 -0300

    Feat: Atividade 07 finalizada

---
+++ 07-Atividade/07-Atividade/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num;
+Console.WriteLine("Digite um número :");
+num = int.Parse(Console.ReadLine());
+if (num <= 10) 
+{ 
+    Console.WriteLine("Não é maior que 10!");
+}
+else
+{ 
+    Console.WriteLine("É maior que 10!"); 
+}
+ 
+    
---
+++ Atividade07/Atividade07/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+
+int num;
+Console.WriteLine("Digite um número:");
+num = int.Parse(Console.ReadLine());
+if (num < 10) ;
+     
+
+
+else
+    Console.WriteLine("É maior que 10! ");
+
