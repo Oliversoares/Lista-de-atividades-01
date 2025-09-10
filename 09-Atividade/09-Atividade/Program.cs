ca7bb29346496b4f795f72f3cc2d2a3dd4054857
Oliversoares <luisguilhermesoares10012009@gmail.com>
2025-09-10 09:10:55 -0300

    Feat : Atividade 9 realizada.

---
+++ 09-Atividade/09-Atividade/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+int nota1;
+int nota2;
+int media;
+Console.WriteLine("Digite suas notas:");
+nota1 = int.Parse(Console.ReadLine());
+nota2 = int.Parse(Console.ReadLine());
+media = nota1 + nota2;
+if (media / 2 >= 6) 
+{
+    Console.WriteLine("APROVADO , média satisfatoria");
+}
+else
+{
+    Console.WriteLine("REPROVADO , média insuficiente");
+}
