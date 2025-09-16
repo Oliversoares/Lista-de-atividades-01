1107a57a3a7a013b3e77ba1b084a50941b234b0d
Oliversoares <luisguilhermesoares10012009@gmail.com>
2025-09-16 08:52:19 -0300

    Feat: Atividade 15 , finalizada

---
+++ 15-Atividade/15-Atividade/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num;
+num = 2000;
+int contador = 1000;
+while (contador <= num)
+{
+    Console.WriteLine($"{contador}");
+    Console.WriteLine($" se dividido por 11 tem resto 5 :{contador % 11 == 5}");
+    contador++;
+}
