1653307ba16fce56da1bdcb1c6ff3a9678f094be
Oliversoares <luisguilhermesoares10012009@gmail.com>
2025-09-16 08:17:05 -0300

    Feat: Atividade 14 , finalizada

---
+++ 14-Atividade/14-Atividade/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num;
+Console.WriteLine("Digite um número maior que ZERO : ");
+num = int.Parse(Console.ReadLine());
+int contador = 1;
+while (contador <= num)
+{
+    Console.WriteLine($"{contador}");
+    contador++;
+}
